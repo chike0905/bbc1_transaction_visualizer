8f8a58495b32ba0063e4a7fa81fd17ce08e79fb7
chike <chike@sfc.wide.ad.jp>
2018-02-07 14:08:34 +0900

    show ref tx but not write edge

--- js/visualize.js
+++ js/visualize.js
@@ -4,7 +4,7 @@ function showtx(){
     asset_group_id = "994db1b032803b760e40a6094856ad2411f5f134e9e08fb5ebb968e6442098b5";
     tx_id = "7599c8dc204b94c96dad9880f56e98e4d0efb6d5d487538a8a3216699f2ae1a8";
     //tx_id = "61c9e3c536c0ff4901ea955e187c00d3c05604d8c8d46112ded50a190a859410";
-    user_id = "2b08d6d7d7cfa1544f12263187b895c1676d9be8282d0f743b89cdfd69fb5a66";
+    user_id = "112b08d6d7d7cfa1544f12263187b895c1676d9be8282d0f743b89cdfd69fb5a66";
     /*
 	asset_group_id = $("#assetgroupid")[0].value;
 	tx_id = $("#tx_id")[0].value;
@@ -39,9 +39,16 @@ function draw(result){
 	var g = new dagreD3.graphlib.Graph({ compound: true })
 						.setGraph({ rankdir: "LR" })
 						.setDefaultEdgeLabel(function() { return {}; });
-
-	g.setNode(0,  {label: "Transaction", clusterLabelPos:"top", style: "stroke: #333; fill: #7f7;"})
-	draw_result(g, 1, result, 0);
+    var counter = 0;
+	var txcounterid = 0
+    result.forEach(function(tx){
+        g.setNode(counter,  {label: "Transaction", clusterLabelPos:"top", style: "stroke: #333; fill: #7f7;"})
+        txcounterid = counter;
+        console.log(txcounterid);
+        counter++;
+        counter = draw_result(g, counter, tx, txcounterid);
+    });
+    //g.setEdge(0, 33);
 
 	var render = new dagreD3.render();
 	var svg = d3.select("svg");
---
+++ post.php
@@ -0,0 +1,51 @@
+<?php
+
+$input_str = file_get_contents("php://input");
+$input_json = json_decode($input_str,true);
+/*
+
+$asset_group_id =   "994db1b032803b760e40a6094856ad2411f5f134e9e08fb5ebb968e6442098b5";
+$tx_id =            "7599c8dc204b94c96dad9880f56e98e4d0efb6d5d487538a8a3216699f2ae1a8";
+$user_id =          "2b08d6d7d7cfa1544f12263187b895c1676d9be8282d0f743b89cdfd69fb5a66";
+*/
+
+$asset_group_id = $input_json["asset_group_id"];
+$tx_id =          $input_json["tx_id"];
+$user_id =        $input_json["user_id"];
+
+$result = get_all_reftx($asset_group_id, $tx_id, $user_id);
+echo json_encode($result);
+
+function get_all_reftx($asset_group_id, $tx_id, $user_id){
+    $result[] = get_tx($asset_group_id, $tx_id, $user_id);
+    if(count($result[0]->Reference) != 0){
+        foreach($result[0]->Reference as $ref){
+            $result[] = get_tx($ref->asset_group_id, $ref->transaction_id, $user_id);
+        }
+    }
+    return $result;
+}
+
+
+function get_tx($asset_group_id, $tx_id, $user_id){
+    $url = 'http://localhost:9000';
+    $data = ["jsonrpc" => "2.0",
+            "method"=> "bbc1_GetTransaction",
+            "params"=> [
+                "asset_group_id" => $asset_group_id,
+                "tx_id" => $tx_id,
+                "user_id" => $user_id
+            ],
+            "id" => 5000
+        ];
+
+    $options = ['http' => [
+                'method'  => 'POST',
+                'content' => json_encode($data),
+                'header'  => "Content-Type: application/json\r\n" .
+                             "Accept: application/json\r\n"
+                 ]];
+    $options = stream_context_create($options);
+    $contents = json_decode(file_get_contents($url, false, $options));
+    return $contents->result;
+}
